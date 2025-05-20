dd4e2b04b2b4b236d30bf20fce8b57c05b548297
zichiLian <emiyaorder@foxmail.com>
2025-05-20 16:14:58 +0800

    fix:db.tsx

--- app/db.tsx
+++ app/db.tsx
@@ -5,7 +5,7 @@ import mysql from 'mysql2/promise';
 function getConfig() {
     return {
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
+        port:Number(process.env.DB_PORT) || 3306,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE
