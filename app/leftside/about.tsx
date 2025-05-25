a3cd61ec05a838b297c14cbac949cef15ee476ac
zichiLian <emiyaorder@foxmail.com>
2025-05-25 19:30:21 +0800

    feat:more beautiful and useful about page(#23)

--- app/leftside/about.tsx
+++ app/leftside/about.tsx
@@ -4,23 +4,30 @@ const About = () => {
     return (
         <>
 
-        <h1>
+        <h1 className="about_title">
             关于本站
         </h1>
-            <div>
+            <div className="about_card">
                 <p>
                 这是一个新手测试博客，没什么技术含量的站点
                 </p>
-                <p>
-                    文章内容目前没有太多想法，只是新手，也正常，有其他问题可以联系我
-                </p>
                 <h1>
-                    联络方式：
-                    xxxxxxxx
-                    xxxxxxxx
-                    xxxxxxxx
+                    联系方式：
                 </h1>
+                <p>邮箱2362982442@qq.com</p>
+                <a href='https://space.bilibili.com/24442474' target="_blank" rel="noopener noreferrer">
+                    → 点击关注我的哔哩哔哩主页 ←
+                </a>
             </div>
+        <div className="about_card">
+              <h1 className="about_title">关于我</h1>
+            <p>
+                如你所见，我是一个刚入行的萌新，有什么测试出来的BUG可以尽管联系我
+            </p>
+            <p>
+                友链随时欢迎加入，联系我
+            </p>
+        </div>
         </>
     );
 };
