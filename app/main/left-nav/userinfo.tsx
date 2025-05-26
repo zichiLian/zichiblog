6e72a99e1c1572c1a62d119f7369c4b14c5e2caf
zichiLian <emiyaorder@foxmail.com>
2025-05-26 22:43:06 +0800

    feat:useful Link page and style(#29)

--- app/main/left-nav/userinfo.tsx
+++ app/main/left-nav/userinfo.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 
 
+function Img(props: { src: string }) {
+  return null;
+}
+
 export default function Userinfo() {
   return (
   <>
     <div className="photo">
-    <img src="/head.webp"/><span>😊</span>
+    <img src="https://avatars.githubusercontent.com/u/202113216"/><span>😊</span>
     </div>
 <h1 className="info-name">怪猫的博客</h1>
 <p className="info-text">但愿人长久，千里共婵娟</p>
