8c90b609c5964ab3bec08769521f88577e63bc58
zichiLian <emiyaorder@foxmail.com>
2025-05-27 22:28:38 +0800

    refactor: navbar remake #53

--- app/main/left-nav/userinfo.tsx
+++ app/main/left-nav/userinfo.tsx
@@ -9,8 +9,8 @@ export default function Userinfo() {
     <div className="photo">
     <img src="https://avatars.githubusercontent.com/u/202113216"/><span>😊</span>
     </div>
-<h1 className="info-name">怪猫的博客</h1>
-<p className="info-text">但愿人长久，千里共婵娟</p>
+ <h1 className="info-name">怪猫的博客</h1>
+ <p className="info-text">但愿人长久，千里共婵娟</p>
 </>
   )
 }
